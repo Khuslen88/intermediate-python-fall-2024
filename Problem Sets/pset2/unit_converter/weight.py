c43b84d56282e1a9b5c406d3d7af2b2fb1e30ed1
Khuslen88 <kzkhuslen@gmail.com>
2024-10-26 22:14:27 +0800

    a

--- Problem Sets/pset2/unit_converter/weight.py
+++
@@ -1,7 +0,0 @@
-def pounds_to_kilograms(pounds):
-    """ Converts pounds to kilograms """
-    return pounds * 0.453592
-
-def kilograms_to_pounds(kilograms):
-    """ Converts kilograms to pounds """
-    return kilograms / 0.453592
